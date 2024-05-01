423ddf5889e18e750f1fbd2a34927910841dfbd9
liamma06 <liam.jbr.ma@gmail.com>
2024-05-01 09:22:39 -0400

    new text

--- CoordArea.java
+++ CoordArea.java
@@ -7,18 +7,24 @@ public class CoordArea {
     //Properties
     JFrame theframe = new JFrame("Quadralateral Coordinate Area Program");
     JPanel thepanel = new JPanel();
-
-    //Methods
-
+    JTextField Ax;
+    JTextField Ay;
+    JLabel A;
 
     //Constructor
     public CoordArea(){
-        thepanel.setPreferredSize(new Dimension(400,200));
+        thepanel.setPreferredSize(new Dimension(700,400));
         thepanel.setLayout(null);
+
+        Ax = new JTextField("0");
+        Ax.setSize(10,10);
+        Ax.setLocation(0,0);
+        thepanel.add(Ax);
 
         //putting the panel inside the frame
         theframe.setContentPane(thepanel);
         theframe.pack();
+        theframe.setResizable(false);
         theframe.setVisible(true);
         theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
